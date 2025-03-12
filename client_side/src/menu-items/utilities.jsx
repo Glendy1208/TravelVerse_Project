aa553c8a2b39c898ea13ce6c4d69ed2ee4de7091
Lealwa <43865171+Lealwa@users.noreply.github.com>
2025-03-13 02:31:59 +0700

    update

--- client_side/src/menu-items/utilities.jsx
+++ client_side/src/menu-items/utilities.jsx
@@ -21,8 +21,8 @@ const icons = {
 // ==============================|| MENU ITEMS - UTILITIES ||============================== //
 
 const utilities = {
-  id: 'utilities',
-  title: 'Utilities',
+  id: 'wisata',
+  title: 'Wisata',
   type: 'group',
   children: [
     {
