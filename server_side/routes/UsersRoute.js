c3d574fe781b1e98426749475aed9fc2f92d74d0
Lealwa <43865171+Lealwa@users.noreply.github.com>
2025-03-18 20:01:36 +0700

    update

--- server_side/routes/UsersRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const usersController= require("../controllers/UsersController");
-
-router.get('/', usersController.getAllUsers);
-
-module.exports = router;
